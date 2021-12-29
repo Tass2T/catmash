e82e8f984458b57d0cdd1aa0aa023faf77aeb5f0
tass2t <brunog90@gmail.com>
2021-12-29 23:01:07 +0100

    added the cats

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home';
@@ -5,11 +7,23 @@ import List from './components/List';
 
 function App() {
 
+  const [data, setData] = useState(false)
+
+  const fetchCatData = () => {
+    axios.get("https://latelier.co/data/cats.json")
+    .then(res => setData(res.data.images))
+    .catch(err => console.warn(err))
+  }
+
+  useEffect(() => {
+    fetchCatData()
+  }, [])
+
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-            <Route path="/" element={<Home/>}/>
+            <Route path="/" element={<Home data={data} setData={setData} />}/>
             <Route path="/list" element={<List/>}/>
         </Routes>
       </BrowserRouter>
