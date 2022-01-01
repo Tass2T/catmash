06e6e0f871114c53c923a4372b87ffac3298249a
tass2t <brunog90@gmail.com>
2022-01-01 16:18:53 +0100

    corrected wrong import

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,5 @@
-import { useEffect } from 'react'
+import { useEffect, useEffect } from 'react'
 import { Link } from 'react-router-dom'
-import { useState } from 'react/cjs/react.development'
 
 export default function Home({ data, setData, count, setCount }) {
 
